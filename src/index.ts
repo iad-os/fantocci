6d0e41ef488f387ee0d3d6f6463e6722dc856893
Daniele Fiungo <daniele.fiungo@iad2.it>
2024-06-05 20:15:54 +0000

    promisify start

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Fantocci } from './Fantocci.js';
 import dotenv from 'dotenv';
+import { Fantocci } from './Fantocci.js';
 import { normalizePort } from './normalizePort.js';
 export * from './oauthFantocci.js';
 export { Fantocci };
@@ -10,18 +10,39 @@ export default async function start(
   host = '0.0.0.0'
 ) {
   const app = await Fantocci();
+  const { promise, resolve, reject } = promiseKeeper<typeof app>();
   app.listen(
     {
-      port: normalizePort(port),
+      port: await normalizePort(port),
       host,
     },
     function (err, address) {
       if (err) {
         app.log.error(err);
+        reject(err);
         process.exit(1);
       }
       app.log.info(`server listening on ${address}`);
+      resolve(app);
     }
   );
-  return app;
+  return promise;
+}
+
+function promiseKeeper<T = unknown>() {
+  let resolve: Parameters<
+      ConstructorParameters<typeof Promise<T>>[0]
+    >[0] = () => {
+      return;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    reject: Parameters<ConstructorParameters<typeof Promise<T>>[0]>[1] = () => {
+      return;
+    };
+
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  return { promise, resolve, reject };
 }
--- src/normalizePort.ts
+++ src/normalizePort.ts
@@ -1,7 +1,7 @@
 /**
  * Normalize a port into a number, string, or false.
  */
-export function normalizePort(val: string | number): number {
+export async function normalizePort(val: string | number): Promise<number> {
   const port = typeof val === 'string' ? parseInt(val, 10) : val;
 
   if (isNaN(port) || port < 0 || port > 65535) {
