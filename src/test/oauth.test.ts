4823148f3fbb568e961a87bd6ba841f514b44474
Daniele Fiungo <daniele.fiungo@iad2.it>
2024-06-11 13:44:47 +0000

    fix and add some tests

--- src/test/oauth.test.ts
+++ src/test/oauth.test.ts
@@ -10,21 +10,18 @@ describe('OAuth2 Test Suite', () => {
     oauthFantocci
   );
   const aValidPayload: FakeToken = {
+    client_id: 'clientId',
+    iss: 'http://myhost',
+    exp: 1234567890,
+    iat: 1234567890,
+    jti: 'jti',
+    aud: 'dev',
+    sub: 'a-man-have-a-subject',
     additional_fake_props: {
       clientId: 'clientId',
       clientSecret: 'clientSecret',
       active: true,
-      diffIssuers: false
     },
-    sub: 'daniele-test',
-    iss: 'sso.fantocci.it/auth/realms/master',
-    active: true,
-    exp: 1321243123,
-    aud: 'cli',
-    azp: 'test-test',
-    iat: 4523433254,
-    cached: false,
-    jti: 'testest'
   };
   const aValidTokenB64 = buildToken(aValidPayload);
 
@@ -45,6 +42,7 @@ describe('OAuth2 Test Suite', () => {
       method: 'POST',
       path: '/introspect',
       headers: {
+        host: 'myhost',
         'content-type': 'application/x-www-form-urlencoded',
         authorization: `Basic ${Buffer.from(
           `${aValidPayload?.additional_fake_props?.clientId}:${aValidPayload?.additional_fake_props?.clientSecret}`
@@ -55,6 +53,69 @@ describe('OAuth2 Test Suite', () => {
     fantocci.log.debug(res, 'Response');
     const { statusCode } = res;
     expect(statusCode).toBe(200);
+    expect(res.json()).toEqual({
+      active: true,
+      aud: 'dev',
+      client_id: 'clientId',
+      exp: 1234567890,
+      iat: 1234567890,
+      iss: 'http://myhost',
+      jti: 'jti',
+      sub: 'a-man-have-a-subject',
+    });
+  });
+  it('fail introspect', async () => {
+    const res = await fantocci.inject({
+      method: 'POST',
+      path: '/introspect',
+      headers: {
+        host: 'not.myhost',
+        'content-type': 'application/x-www-form-urlencoded',
+        authorization: `Basic ${Buffer.from(
+          `${aValidPayload?.additional_fake_props?.clientId}:${aValidPayload?.additional_fake_props?.clientSecret}`
+        ).toString('base64')}`,
+      },
+      payload: `token=${aValidTokenB64}`,
+    });
+    fantocci.log.debug(res, 'Response');
+    const { statusCode } = res;
+    expect(statusCode).toBe(200);
+    expect(res.json()).toEqual({
+      active: false,
+    });
+  });
+  it('fail with 401 if credential clientId is wrong', async () => {
+    const res = await fantocci.inject({
+      method: 'POST',
+      path: '/introspect',
+      headers: {
+        host: 'not.myhost',
+        'content-type': 'application/x-www-form-urlencoded',
+        authorization: `Basic ${Buffer.from(
+          `not${aValidPayload?.additional_fake_props?.clientId}:${aValidPayload?.additional_fake_props?.clientSecret}`
+        ).toString('base64')}`,
+      },
+      payload: `token=${aValidTokenB64}`,
+    });
+    fantocci.log.debug(res, 'Response');
+    const { statusCode } = res;
+    expect(statusCode).toBe(401);
+  });
+  it('fail with 401 if credential clientSecret is wrong', async () => {
+    const res = await fantocci.inject({
+      method: 'POST',
+      path: '/introspect',
+      headers: {
+        host: 'not.myhost',
+        'content-type': 'application/x-www-form-urlencoded',
+        authorization: `Basic ${Buffer.from(
+          `${aValidPayload?.additional_fake_props?.clientId}:not${aValidPayload?.additional_fake_props?.clientSecret}`
+        ).toString('base64')}`,
+      },
+      payload: `token=${aValidTokenB64}`,
+    });
+    fantocci.log.debug(res, 'Response');
+    const { statusCode } = res;
+    expect(statusCode).toBe(401);
   });
 });
-
